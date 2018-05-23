43773d5e26d863a720cb9f0aa32c4280a5501a46
Eric Dube <eric.alex.dube@gmail.com>
2018-05-23 18:03:32 -0400

    Add gostrings.AtomicReplace

---
+++ gostrings/atomicreplace.go
@@ -0,0 +1,51 @@
+package gostrings
+
+// AtomicReplace replaces multiple substrings in a string atomically. Here,
+// atomically means it will replace all the substrings without replacing
+// instances which occur after replacing other substrings.
+func AtomicReplace(
+	input string, replacements map[string]string,
+) (count map[string]int, output string) {
+	count = map[string]int{}
+	output = ""
+
+	// Map input string index to substring being replaced
+	replacementIndexes := map[int]string{}
+
+	// Iterate over all the strings being replaced and populate the map of
+	// indexes that need replacements
+	for key := range replacements {
+		indices := IndexAll(input, key)
+		for _, index := range indices {
+			replacementIndexes[index] = key
+		}
+		// Ensure this key is added to the map of counts
+		count[key] = 0
+	}
+
+	// Take a sip of coffee
+	// (this didn't require writing any code)
+
+	// Idea: using a sorted list of replacement keys here may allow for a more
+	// efficient replacement algorithm, depending on how Golang is optimized.
+
+	// Perform replacements
+	linput := len(input)
+	for i := 0; i < linput; i++ {
+		if _, exists := replacementIndexes[i]; exists {
+			// Get the original and replacement substrings for this index
+			replaceFrom := replacementIndexes[i]
+			replaceTo := replacements[replaceFrom]
+			// Add replacement to output and scan past original substring
+			output += replaceTo
+			i += len(replaceFrom) - 1 // (-1 because of loop increment)
+			// Update replacement count
+			count[replaceFrom]++
+		} else {
+			// Add the character at this index to the output string
+			output += input[i : i+1]
+		}
+	}
+
+	return // NB: named return values
+}
---
+++ gostrings/atomicreplace_test.go
@@ -0,0 +1,49 @@
+package gostrings_test
+
+import (
+	"testing"
+
+	"github.com/Investabit/investabit-go/stringutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAtomicReplace(t *testing.T) {
+	type Case struct {
+		// Independant variables
+		input        string
+		replacements map[string]string
+		// Dependant variables
+		count  map[string]int
+		output string
+	}
+	cases := []Case{
+		// Case with longer replacement
+		Case{
+			input:  "fuzzy wuzzy wuzza bear",
+			output: "fuzzy becausey becausea bear",
+			count: map[string]int{
+				"wuzz": 2,
+			},
+			replacements: map[string]string{
+				"wuzz": "because",
+			},
+		},
+		// Case with shorter replacement
+		Case{
+			input:  "fuzzy wuzzy wuzza bear",
+			output: "fuzzy wuzy wuza bear",
+			count: map[string]int{
+				"wuzz": 2,
+			},
+			replacements: map[string]string{
+				"wuzz": "wuz",
+			},
+		},
+	}
+
+	for _, cse := range cases {
+		count, output := stringutil.AtomicReplace(cse.input, cse.replacements)
+		assert.EqualValues(t, cse.output, output, "output must match")
+		assert.EqualValues(t, cse.count, count, "count must match")
+	}
+}
