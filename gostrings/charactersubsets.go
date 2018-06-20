4fca23dc4b3a22c3f17dd1ca7ae772bf575bad2a
Eric Dube <eric.alex.dube@gmail.com>
2018-06-20 17:43:39 -0400

    Add constants for miscellaneous character subsets

---
+++ gostrings/charactersubsets.go
@@ -0,0 +1,14 @@
+package gostrings
+
+// These constants define miscellaneous character subsets
+const (
+	AlphabetLowerQWERTY = "qwertyuiopasdfghjklzxcvbnm"
+	AlphabetLower       = "abcdefghijklmnopqrstuvwxyz"
+	AlphabetUpperQWERTY = "QWERTYUIOPASDFGHJKLZXCVBNM"
+	AlphabetUpper       = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	Alphabet = AlphabetUpper + AlphabetLower
+
+	Digits       = "0123456789"
+	DigitsQWERTY = "1234567890"
+)
